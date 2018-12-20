dc327208dc15efef0e6583a8d1d4668d16858f73
thientynguyen <nttthiencnttdn@gmail.com>
2018-12-20 22:35:58 +0700

    middleware admin

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin/login','Admin\LoginController@showLoginForm')->name('adminlogin');
 Route::post('/admin/login','Admin\LoginController@checkLogin')->name('adminlogin');
 
-//thiếu middleware dể phân quyền cho admin
-Route::group(['prefix' => 'admin'], function() {
+//thiếu middleware để phân quyền cho admin
+// group của admin mới vào được
+Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function() {
 
     Route::get('/home/{user}', 'Admin\LoginController@home')->name('admin.home');
 
